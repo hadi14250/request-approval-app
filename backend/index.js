312960611edb5ecf87afc0ed89d95963cc64c270
hadi14250 <hadihk2k@gmail.com>
2025-12-19 21:44:12 +0400

    finished creating requests endpoint

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,37 @@ app.use(express.json());
 let requests = [];
 let nextId = 1;
 
+const users = [
+    { id: 1, name: "Hadi", roles: ["Requester"] },
+    { id: 2, name: "Haneen", roles: ["Approver"] },
+    { id: 3, name: "Lama", roles: ["Approver", "Requester"] }
+];
+
+function getCurrentUser(req) {
+    const userId = Number(req.headers["user-id"]);
+
+    if (!userId) return null;
+
+    return users.find( (user) => user.id === userId ) || null
+
+}
+
 app.get("/health", (req, res) => {
     res.json({ status : "worked"})
 });
 
 app.post("/requests", (req, res) => {
+
+  const user  = getCurrentUser(req);
+
+  if (!user) {
+    return res.status(401).json({ error: "Missing user-id in header" });
+  }
+
+  if (!user.roles.includes("Requester")) {
+    return res.status(401).json({ error: "Only Requesters can create requests" });
+  }
+
   const { title, description, type } = req.body;
 
   if (!title) {
@@ -22,8 +48,13 @@ app.post("/requests", (req, res) => {
     id: nextId,
     title: title,
     description: description || null,
-    type: type || general,
+    type: type || "General",
     status: "Draft",
+    createdByUserId: user.id,
+    createdByUserName: user.name,
+    approverComment: null,
+    approvedByUserId: null,
+    approvedByUserName: null,
     createdAt: new Date().toISOString(),
     updatedAt: new Date().toISOString()
   }
@@ -31,7 +62,7 @@ app.post("/requests", (req, res) => {
   requests.push(newRequest);
   nextId++;
 
-  res.status(200).json(newRequest);
+  res.status(201).json(newRequest);
 
   console.log("New requests array:", requests);
 })
